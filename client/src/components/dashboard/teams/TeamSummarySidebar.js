720b1e571ff89e71c446f5d2a3087c3b8a152b45
roberts98 <robertsidzinka@interia.pl>
2020-01-12 01:26:58 +0100

    style team sidebar

--- client/src/components/dashboard/teams/TeamSummarySidebar.js
+++ client/src/components/dashboard/teams/TeamSummarySidebar.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import {
+  FONT_HUGE,
+  FONT_BIG,
+  XXS_SPACING,
+  MEDIUM_SPACING
+} from '../../../contants/styles';
+
+const Name = styled.h1`
+  font-size: ${FONT_HUGE};
+  margin-bottom: ${MEDIUM_SPACING};
+`;
+
+const Heading = styled.h2`
+  font-size: ${FONT_BIG};
+  font-weight: 700;
+  margin-bottom: ${XXS_SPACING};
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding-left: 0;
+`;
 
 function TeamSummarySidebar({ team }) {
   return (
     <div>
-      <h1>{team.name}</h1>
-      <h2>Description</h2>
+      <Name>{team.name}</Name>
+      <Heading>Description</Heading>
       <p>{team.description}</p>
-      <h2>Memebrs</h2>
-      <ul>
+      <Heading>Memebrs</Heading>
+      <List>
         {team.members.map(member => (
           <li key={member._id}>{member.username}</li>
         ))}
-      </ul>
+      </List>
     </div>
   );
 }
